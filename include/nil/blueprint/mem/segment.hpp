8df68c1da72c37916264c404a242f8820fa317ea
Alexander Evgin <aleasims@gmail.com>
2024-01-10 20:06:38 +0000

    Some implementations for mem::segment

--- include/nil/blueprint/mem/segment.hpp
+++ include/nil/blueprint/mem/segment.hpp
@@ -21,17 +21,18 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //---------------------------------------------------------------------------//
-// @file This file defines memory segments, used in assigner memory model, and
-// segment map, which maps segments into arbitrary data.
+// @file This file defines memory segments, used in assigner memory model.
 //---------------------------------------------------------------------------//
 
 #ifndef CRYPTO3_ASSIGNER_MEM_SEGMENT_HPP
 #define CRYPTO3_ASSIGNER_MEM_SEGMENT_HPP
 
 #include <iostream>
+#include <sstream>
 
 #include <boost/format.hpp>
 
+#include <nil/blueprint/asserts.hpp>
 #include <nil/blueprint/mem/layout.hpp>
 
 namespace nil {
@@ -45,32 +46,116 @@ namespace nil {
                 /// Size in bytes.
                 size_type size;
 
-                segment() : pointer(0), size(0) {
-                }
+                segment() = default;
 
                 segment(ptr_type pointer, size_type size) : pointer(pointer), size(size) {
                 }
 
-                /// Whether this segment contains given pointer or not.
-                bool contains(ptr_type& ptr) const {
-                    return ((pointer <= ptr) && (pointer + size > ptr));
+                segment(const segment& other) {
+                    pointer = other.pointer;
+                    size = other.size;
                 }
 
                 bool operator==(const segment& other) const {
                     return ((this->pointer == other.pointer) && (this->size == other.size));
                 }
 
+                bool operator!=(const segment& other) const {
+                    return !operator==(other);
+                }
+
                 bool operator<(const segment& other) const {
-                    return (this->pointer + size < other.pointer);
+                    return (this->pointer + size <= other.pointer);
+                }
+
+                bool operator>(const segment& other) const {
+                    return (other.pointer + other.size <= this->pointer);
+                }
+
+                /// Return printable view of the segment.
+                std::string print_string() const {
+                    std::stringstream buffer;
+                    buffer << boost::format("0x%016x+%08x") % this->pointer % this->size;
+                    return buffer.str();
+                }
+
+                /// Whether this segment contains given pointer or not.
+                bool contains(ptr_type ptr) const {
+                    return ((pointer <= ptr) && (pointer + size > ptr));
+                }
+
+                /// Whether given segment is a subsegment of this segment or not.
+                bool contains(segment& other) const {
+                    return ((pointer <= other.pointer) && (pointer + size >= other.pointer + other.size));
+                }
+
+                /// Whether this segment intersects with other segment.
+                bool intersects(segment& other) const {
+                    return !((*this < other) || (*this > other));
                 }
             };
 
             std::ostream& operator<<(std::ostream& out, segment const& seg) {
-                out << boost::format("0x%016x+%08x") % seg.pointer % seg.size;
-                return out;
+                return out << seg.print_string();
             }
         }    // namespace mem
     }        // namespace blueprint
+}    // namespace nil
+
+namespace nil {
+    namespace blueprint {
+        namespace mem {
+            namespace tests {
+                void test_segment_equality() {
+                    segment a(0, 4);
+                    segment b;
+                    b.pointer = 0;
+                    b.size = 4;
+                    segment c(0, 8);
+                    ASSERT(a == b);
+                    ASSERT(a != c);
+                }
+
+                void test_segment_compare() {
+                    segment a(0, 4);
+                    segment b(4, 4);
+                    segment c(0, 8);
+                    ASSERT(a < b);
+                    ASSERT(b > a);
+                    ASSERT(!(a < c));
+                }
+
+                void test_segment_contains() {
+                    segment a(0, 4);
+                    ASSERT(a.contains(0));
+                    ASSERT(a.contains(1));
+                    ASSERT(!a.contains(4));
+                }
+
+                void test_segment_contains_segment() {
+                    segment a(0, 4);
+                    segment b(1, 2);
+                    segment c(0, 5);
+                    ASSERT(a.contains(b));
+                    ASSERT(!a.contains(c));
+                }
+
+                void test_segment_intersects() {
+                    segment a(0, 4);
+                    segment b(0, 16);
+                    segment c(4, 4);
+                    ASSERT(a.intersects(b));
+                    ASSERT(!a.intersects(c));
+                    ASSERT(b.intersects(c));
+                }
+
+                void test_segment_print_string() {
+                    segment a(0xffffffff00000000, 0xaaaaaaaa);
+                    ASSERT(a.print_string() == std::string("0xffffffff00000000+aaaaaaaa"));
+                }
+            }    // namespace tests
+        }        // namespace mem
+    }            // namespace blueprint
 }    // namespace nil
 
 #endif    // CRYPTO3_ASSIGNER_MEM_SEGMENT_HPP
